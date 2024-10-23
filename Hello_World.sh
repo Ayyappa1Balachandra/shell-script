ffccc0e6a9a0b4be772c08f82adc602bd874302d
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 22:20:48 +0530

    Hello World Script added

--- Hello_World.sh
+++ Hello_World.sh
@@ -2,3 +2,7 @@
 
 echo "Hello i am learning devops"
 echo "Hello mama"
+echo "Bala:Hi Chandu, good morning"
+echo "Chandu:Hi Bala,good morning"
+echo "Bala:Hoe are Chandu"
+echo "Chandu:I am  fineBala"
