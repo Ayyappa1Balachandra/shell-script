f22743892ce5965701798ac11d7207bc31780541
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 22:40:13 +0530

    Hello World Script added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
-PERSON1=$1
-PERSON2=$2
+PERSON1=$1 # we can pass the values from command line without hard coreing the values.
+PERSON2=$2 # we can pass the values from command line without hard coreing the values.
 echo "$PERSON1:Hi $PERSON2, good morning"
 echo "$PERSON2:Hi $PERSON1,good morning"
 echo "$PERSON1:Hoe are $PERSON2"
