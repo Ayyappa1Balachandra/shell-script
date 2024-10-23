9ceb18b836e414c2d4231cb7c929a0f56525487c
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 23:05:56 +0530

    Hello World Script added

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#this is called array,instead of single value it can hold multiple values.
+
+#PERSONS=("RAMESH""SURESH""SACHIN")
+PERSONS=
+#Inside array we always refer values with index and it starts from 0
+
+echo "First Person:${PERSONS[0]}"
+echo "All Person:${PERSONS[@]}"
+
+# @ refers or include all values in arrays
