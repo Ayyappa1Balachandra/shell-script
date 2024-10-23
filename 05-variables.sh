3a8c9accfddea6903ffa810de6d5a29efd3dd23b
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 22:58:40 +0530

    Hello World Script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username"
-read USERNAME #this is the variable where the value of the one user entered
+read -s USERNAME #this is the variable where the value of the one user entered
 
-#echo "Username is entered is :$USERNAME"
+echo "Username is entered is :$USERNAME"
 echo "Please enter your password"
-read PASSWORD #this is the variable where the value of the one user entered
-#echo "Password entered is :$PASSWORD"
+read -s PASSWORD #this is the variable where the value of the one user entered
+echo "Password entered is :$PASSWORD"
