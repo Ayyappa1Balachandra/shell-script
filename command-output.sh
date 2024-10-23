a7c95b2b9e25ee7de796ca7f3ccff20724c2dbc7
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 22:33:42 +0530

    Hello World Script added

--- command-output.sh
+++ command-output.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 DATE=$(date)
+#What shell will do means, it will run the command date and store the output in DATE variable
+
 echo "Hello, this script is executed at timestamp:$DATE"
