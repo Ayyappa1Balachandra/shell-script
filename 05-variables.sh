90ba9705b21b3c936976c357c9a4567fc7a90e2c
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-23 22:54:22 +0530

    Hello World Script added

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username"
+read USERNAME #this is the variable where the value of the one user entered
+
+echo "Username is entered is :$USERNAME"
+echo "Please enter your password"
+read PASSWORD #this is the variable where the value of the one user entered
+echo "Password entered is :$PASSWORD"
