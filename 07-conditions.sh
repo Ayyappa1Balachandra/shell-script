2b0b5bb3105ed6f0d8a1a28535c3879c8b6abc87
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-24 21:03:56 +0530

    conditions added

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -7,10 +7,10 @@ NUMBER1=$1
 # -ge greater-than or equal-to -ge is operator
 #-gt only grater-than its also an operator
 
-if[$NUMBER -gt ]
-    then
-        echo "$NUMBER is greater than 10"
+if[$NUMBER -gt 10]
+then
+    echo "$NUMBER is greater than 10"
 
-    else
-        echo "$NUMBER is not greater than 10"
-    fi    
+else
+    echo "$NUMBER is not greater than 10"
+fi    
