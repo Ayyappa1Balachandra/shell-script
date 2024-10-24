7ec5df84395f50da8af61462b11ce7e18e2210b1
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-24 20:57:35 +0530

    conditions added

---
+++ 07-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#print if the number is greater than 10 or not
+NUMBER1=$1
+
+# -ge greater-than or equal-to -ge is operator
+#-gt only grater-than its also an operator
+
+if[$NUMBER -ge ]
+then
+    echo "$NUMBER is greater than 10"
+
+else
+    echo "$NUMBER is not greater than 10"
+fi    
