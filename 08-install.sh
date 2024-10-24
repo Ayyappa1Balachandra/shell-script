557fbe37005dd3c46a82543683ba6bd42d2ca467
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-24 21:39:53 +0530

    check the installations

--- 08-install.sh
+++ 08-install.sh
@@ -7,10 +7,9 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then 
     echo "ERROR:: Please run this script with root access"
-else
-    
-    #echo "INFO:: You are root user"
-
+#else
+     #echo "INFO:: You are root user"
+     #remember this if ur not using else part then else also comment ok.....
 fi
 
 yum install mysql -y
