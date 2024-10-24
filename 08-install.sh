2395abe4535ce7b680a60949eba212e58c0e34f1
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-24 21:49:17 +0530

    check the installations

--- 08-install.sh
+++ 08-install.sh
@@ -4,7 +4,7 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: Please run this script with root access"
     exit 1
@@ -14,5 +14,13 @@ then
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
 
 
