0ce39eca96156b5c2ab319cd41a987db748662d7
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-26 17:24:44 +0530

    delete added

---
+++ 10-deletepacages.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LIST_OF_APPS=""
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR $N:: Please run this script with root access"
+    exit 1
+#else
+     #echo "INFO:: You are root user"
+     #remember this if ur not using else part then else also comment ok.....
+fi
+
+#this function concept syntax is VALIDATE(){
+#WRITE YOUR LOGIC HERE.
+#}
+
+VALIDATE(){
+    #$1--> it will rceive the arugment1
+
+    if [ $LIST_OF_APPS -ne 0 ]
+    then
+        echo "Installation .....$R FAILURE $N"
+        exit 1
+    else
+        echo "Installation .....$G SUCCESS $N"
+    fi
+}
+
+
+#yum install mysql -y
+
+#VALIDATE $? "$Y mysql Installing"
+
+#yum install postfix -y
+
+#VALIDATE $? "$Y postfix Installing"
+
+yum remove $LIST_OF_APPS -y
+
+VALIDATE $? " Installing $LIST_OF_APPS"
