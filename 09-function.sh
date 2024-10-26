bdcb4a2ca90dd4ebb51403bd8f181e39c23acc66
Ayyappa1Balachandra <ayyappabalachandra@gmail.com>
2024-10-26 19:05:46 +0530

    special-variable changes

--- 09-function.sh
+++ 09-function.sh
@@ -2,15 +2,18 @@
 
 #this function should validate the previous command and inform user it is success or failure.
 
-DATE=$(date +%F-%H-%M-%S)
-LOGFILE=/tmp/
-
-USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+DATE=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+# $DATE.log $DATE means we get DATE of excution and .log means we get log file of the files.
+
+# Now I should append all output to the '''''''$LOGFILE'''''''... 
 
 #this function concept syntax is VALIDATE(){
 #WRITE YOUR LOGIC HERE.
@@ -29,6 +32,9 @@ VALIDATE(){
     fi
 }
 
+USERID=$(id -u)
+
+
 if [ $USERID -ne 0 ]
 then 
     echo -e "$R ERROR $N:: Please run this script with root access"
@@ -40,10 +46,10 @@ fi
 
 
 
-yum install mysql -y
+yum install mysql -y &>>LOGFILE
 
 VALIDATE $? "$Y mysql Installing"
 
-yum install postfix -y
+yum install postfix -y &>>LOGFILE
 
 VALIDATE $? "$Y postfix Installing"
